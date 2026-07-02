917579f04adeb7af084902bc0b15eff967cdc737
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-07-02 13:44:46 +0800

    add doc explaining neoforge version number

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -2,8 +2,20 @@
 import { defineConfig } from 'astro/config'
 
 import starlight from '@astrojs/starlight'
+import remarkMath from 'remark-math'
+import rehypeKatex from 'rehype-katex'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [starlight({ title: 'Creeper Registry' })],
+  markdown: {
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
+  },
+
+  integrations: [
+    starlight({
+      title: 'Creeper Registry',
+      customCss: ['katex/dist/katex.min.css'],
+    }),
+  ],
 })
